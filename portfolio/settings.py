d4c746957d8ee90e0acfa692595cf2dd972e488c
Rajneesh Kumar <rajneesh.coder@gmail.com>
2025-01-14 12:17:53 +0530

    whitenoise added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
 #=================================
 MIDDLEWARE += [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 #=================================
@@ -138,9 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
